75d9ba9ccdb03dafc97f6a834df0f1972b239cd2
bioforesight <144956903+bioforesight@users.noreply.github.com>
2025-04-11 01:04:06 +0300

    Delete weather_app directory

--- weather_app/src/main/java/com/example/weather_app/location/repository/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.weather_app.location.repository;
-
-import com.example.weather_app.location.model.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location, Integer> {
-}
--- weather_app/src/main/java/com/example/weather_app/location/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.weather_app.location.repository;
-
-import com.example.weather_app.location.model.Person;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends CrudRepository<Person, Integer> {
-}
